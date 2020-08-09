08e5cbb341159256f641e9ac4b700bca49ba9288
Nick Randolph <nick@builttoroam.com>
2020-08-09 17:47:35 +1000

    Removing wasm partial class

--- WebContentSample/WebContentSample.Shared/MarkedControl.cs
+++ WebContentSample/WebContentSample.Shared/MarkedControl.cs
@@ -1,27 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Uno.Extensions;
+using Uno.Foundation;
+using Uno.Foundation.Interop;
 using Windows.UI.Xaml.Controls;
 
 namespace WebContentSample
 {
+    public partial class MarkedControl :
 #if !__WASM__
-    public partial class MarkedControl : UserControl
+UserControl
+#else
+        Control, IJSObject
+#endif
     {
-        private readonly WebView internalWebView;
         public event EventHandler MarkedReady;
+
+#if !__WASM__
+        private readonly WebView internalWebView;
+#else
+        private readonly JSObjectHandle _handle;
+        JSObjectHandle IJSObject.Handle => _handle;
+#endif
+
         public MarkedControl()
         {
+#if !__WASM__
             Content = internalWebView = new WebView();
-
             internalWebView.NavigationCompleted += NavigationCompleted;
+#else
+            _handle = JSObjectHandle.Create(this);
+#endif
             Loaded += MarkedControl_Loaded;
         }
 
         private void MarkedControl_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
+#if !__WASM__
             var html = @"<html>
      <body>
        <div id = ""content"">This is default text</ div>
@@ -29,32 +47,51 @@ namespace WebContentSample
     </ html>
 ";
             internalWebView.NavigateToString(html);
+#else
+            MarkedReady?.Invoke(this, EventArgs.Empty);
+#endif
         }
 
+
+#if !__WASM__
         private void NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
         {
             MarkedReady?.Invoke(this, EventArgs.Empty);
         }
+#endif
 
-        public void AddWebAllowedObject(string name, object pObject)
-        {
-            internalWebView.AddWebAllowedObject(name, pObject);
-        }
-
-        public Task<string> InvokeScriptAsync(string scriptName, IEnumerable<string> arguments)
+        public async Task InvokeScriptAsync(string scriptName, IEnumerable<string> arguments)
         {
             var newArguments = new List<string>();
             foreach (var arg in arguments)
             {
-               var outArg = PrepareArgument(arg);
+                var outArg = ReplaceLiterals(arg);
                 newArguments.Add(outArg);
             }
+
+
+#if !__WASM__
             var source = new CancellationTokenSource();
             return internalWebView.InvokeScriptAsync(
 #if !WINDOWS_UWP
                 source.Token,
 #endif
                 scriptName, newArguments.ToArray()).AsTask();
+#else
+            var script = $"javascript:{scriptName}(\"{newArguments.FirstOrDefault()}\");";
+            Console.Error.WriteLine(script);
+
+            try
+            {
+                var result = WebAssemblyRuntime.InvokeJS(script);
+                Console.WriteLine($"Result: {result}");
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("FAILED " + e);
+            }
+
+#endif
         }
 
         private static Func<string, string> ReplaceLiterals = txt =>
@@ -62,13 +99,7 @@ namespace WebContentSample
         txt;
 #else
         txt.Replace("\\", "\\\\").Replace("\n", "\\n").Replace("\r", "\\r").Replace("\"", "\\\"").Replace("\'", "\\\'").Replace("`", "\\`").Replace("^", "\\^");
-#endif 
-
-        private string PrepareArgument(string argument)
-        {
-            if (argument == null) return argument;
-            return ReplaceLiterals(argument);
-        }
-    }
 #endif
+
     }
+}
